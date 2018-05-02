96fbc70ca2b2033ca4cb102b1937fb33eef1e327
dskle <vss995@naver.com>
2018-05-02 17:43:32 +0900

    update basic.py

--- basic.py
+++ basic.py
@@ -6,7 +6,7 @@ print "python"
 msg = "hello python"
 print msg
 # 문자열 슬라이싱
-print msg[1:3]
+print msg[0:3]
 print msg[-3:]
 print msg[:-2]
 print msg[::-1]
@@ -22,11 +22,11 @@ print data
 # 리스트 데이터 제거
 data.pop()
 print data
-data.pop
+data.pop()
 print data
 # 리스트 요소 인덱스 검색
 print data.index("hi")
-# index apthem tlfvotl dpfj qkftod
+# index 메소드 실패시 에러 발생
 # print data.index("hi222")
 
 # 사전(딕셔너리)
@@ -37,13 +37,13 @@ user['you'] = {'age':22, 'address':'seoul'}
 # 사전 출력
 print user
 # 사전 데이터 검색 키 활용
-print user['me']
+print user['you' = {'age'}]
 print "user key:", user.keys()
 print "me" in user.keys()
 
 # 제어
 # if, if else, if elif else
-num = 4
+num = 0
 if num > 0:
     print "num > 0"
 
